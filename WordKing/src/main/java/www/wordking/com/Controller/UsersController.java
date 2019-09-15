34adcd671a11d32be02e10e55b535eeeaf7b7e6e
akqhe <akqhe@DESKTOP-9FOAQC0>
2019-09-15 17:58:37 +0900

    123

--- WordKing/src/main/java/www/wordking/com/Controller/UsersController.java
+++ WordKing/src/main/java/www/wordking/com/Controller/UsersController.java
@@ -60,5 +60,31 @@ public class UsersController {
 		return "true";
 	}
 	
+	@RequestMapping(value="/withdraw",method=RequestMethod.GET)
+	@ResponseBody
+	public int withdraw(HttpSession session) {	
+		String id = (String)session.getAttribute("loginId");
+		int result = udao.withdraw(id);
+		session.invalidate();
+		return result;
+	}
 	
+	@RequestMapping(value="/select",method=RequestMethod.POST)
+	@ResponseBody
+	public Users select(HttpSession session) {
+		String id = (String)session.getAttribute("loginId");
+		Users vo = udao.select(id);
+		return vo;
+	}
+	
+	@RequestMapping(value="/userUpdate",method=RequestMethod.POST)
+	@ResponseBody
+	public int userUpdate(Users vo, HttpSession session) {
+		String id = (String)session.getAttribute("loginId");
+		vo.setUserId(id);
+		System.out.println(vo);
+		int result = udao.userUpdate(vo);
+		System.out.println(result);
+		return result;
+	}
 }
